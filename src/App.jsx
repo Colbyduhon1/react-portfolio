ce8f782eb42f105128e2668b9a2b95c62147f745
colbyduhon1 <colby.duhon1@gmail.com>
2018-01-28 13:36:14 -0600

    Initial commit with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import Parallax from 'react-springy-parallax';
+import Header from './Header';
+import Project from './Project';
+
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+    	<Parallax ref='parallax' pages={2}>
+		    <Parallax.Layer
+		        // Page offset, or where the layer will be at when scrolled to
+		        // 0 means start, 1 second page, 1.5 second and half, and so on ...
+		        offset={0}
+		        // Parallax factor, allows for positive and negative values
+		        // Shifts the layer up or down in accordance to its offset
+		        speed={1}
+		                            onClick={() => this.refs.parallax.scrollTo(1)}>
+          		<Header />
+		    </Parallax.Layer>
+		    <Parallax.Layer
+		        // Page offset, or where the layer will be at when scrolled to
+		        // 0 means start, 1 second page, 1.5 second and half, and so on ...
+		        offset={1}
+		        // Parallax factor, allows for positive and negative values
+		        // Shifts the layer up or down in accordance to its offset
+		        speed={1}>
+			<Project />
+		    </Parallax.Layer>
+      	</Parallax>
     );
   }
 }
---
+++ src/Project.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+
+class Project extends Component {
+	render() {
+		return (
+			<div className="introductionContainer">
+				<div className="introductionContent">
+					<div className="introductionName">
+					Colby Duhon
+					</div>
+					<div className="introductionTagline">
+					Full Stack Web Developer
+					</div>
+					<div className="introductionLocationLine">
+					San Francisco
+					</div>
+					<div className="introductionIcons">
+						<a href="https://github.com/colbyduhon1">
+							<i className = "icon fa fa-github"/>
+						</a>
+						<a href="https://www.linkedin.com/in/colby-duhon/">
+							<i className = "icon fa fa-linkedin-square"/>
+						</a>
+					</div>
+				</div>
+			</div>
+			)
+	}
+}
+
+export default Project;
