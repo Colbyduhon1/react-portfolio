700b49757e57a8761fbb90beed2b2723ffa2c370
colbyduhon1 <colby.duhon1@gmail.com>
2018-01-29 15:22:15 -0600

    Changed construction of server

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath
-  })
-  .listen(process.env.PORT || 5000, '0.0.0.0', function (err, result) {
+
+const compiler = webpack(config);
+const devServerOptions = Object.assign({}, config.devServer, {
+  disableHostCheck: true,
+  publicPath: config.output.publicPath
+	})
+
+const server = new WebpackDevServer(compiler, devServerOptions);
+
+
+
+
+  server.listen(process.env.PORT || 5000, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
     }
